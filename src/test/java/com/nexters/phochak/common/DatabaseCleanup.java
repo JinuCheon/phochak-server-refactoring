9d9e0eb00a481db9c038e7e33b205be50c80f6d0
JinuCheon <woojin8787@gmail.com>
2023-08-23 17:23:58 +0900

    4. DatabaseCleanup 에서 USER 예약어를 처리할 수 있도록 변경

--- src/test/java/com/nexters/phochak/common/DatabaseCleanup.java
+++ src/test/java/com/nexters/phochak/common/DatabaseCleanup.java
@@ -55,7 +55,7 @@ public class DatabaseCleanup implements InitializingBean {
 
         for (final String tableName : tableNames) {
             entityManager.createNativeQuery("TRUNCATE TABLE " + tableName).executeUpdate();
-            entityManager.createNativeQuery("ALTER TABLE " + tableName + " ALTER COLUMN ID RESTART WITH 1").executeUpdate();
+            entityManager.createNativeQuery("TRUNCATE TABLE " + tableName + " RESTART IDENTITY ").executeUpdate();
         }
 
         entityManager.createNativeQuery("SET REFERENTIAL_INTEGRITY TRUE").executeUpdate();
